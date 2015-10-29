f431fb8c422bdca13c479e9969c2c18d8026689b
Adib Saad <adib@airpr.com>
2015-10-29 01:58:30 -0400

    some front-end for logging in

---
+++ client/app/routes/login.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  email: null,
+  password: null,
+
+  actions: {
+    authenticate: function(){
+    }
+  }
+});
